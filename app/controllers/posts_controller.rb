f4df5b0c58b9133e2232599264b9f12a4f5886af
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-28 10:41:43 +0100

    pagination wip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.limit(20)
+    if params[:offset] == nil 
+      @offset_value = 0  
+    elsif
+      start = params[:offset].to_i
+      next_page = params[:offset].to_i
+      @start_page_number = start + next_page  
+    end 
+      
+    @posts = Post.offset(@offset_value).limit(20)
+    #variable set by number user clicks
   end
 
   def show
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+
+  def page_counter
+    start = 0 
+    next_page = params[:change_page].to_i
+    page = start + next_page
+    start =+ next_page
+  end
+
 end
